54412cf6b74cf0ac6f97ad44423f335c54f7bc58
Nahom <8nahom8@gmail.com>
2025-04-12 17:19:03 -0500

    added a button to upload files and a text status with the filename

--- src/main.py
+++ src/main.py
@@ -1,1 +1,34 @@
 import pandas as pd
+import customtkinter as ct
+
+filename = "No file uploaded"
+insert = "None"
+data = "None"
+
+app = ct.CTk()
+app.title("Vizzy")
+app.geometry("900x700")
+
+def load_csv():
+     global filename, data
+     filename = ct.filedialog.askopenfilename()
+     if filename.endswith('.csv'):
+          data = pd.read_csv(filename)
+          filenameText.configure(text=filename if filename else "No file uploaded")
+     else:
+          filenameText.configure(text="Please upload a CSV file.")
+
+uploadFilebtn = ct.CTkButton(app, text="Upload CSV", command=load_csv)
+uploadFilebtn.pack()
+
+filenameText = ct.CTkLabel(app, text_color="black", fg_color="transparent", text=filename)
+filenameText.pack()
+
+
+
+
+
+
+
+
+app.mainloop()
