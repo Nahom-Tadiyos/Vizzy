73cc3d4790b32fcf834e38f09bdc3c6262b6bf79
Nahom <8nahom8@gmail.com>
2025-04-12 17:35:15 -0500

    added a preview table so the user can preview their data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from customtkinter import *
 import customtkinter as ct
+import tktable as tb
 
+selected_graph_type = None
 filename = "No file uploaded"
 insert = "None"
 data = "None"
@@ -15,14 +18,34 @@ def load_csv():
      if filename.endswith('.csv'):
           data = pd.read_csv(filename)
           filenameText.configure(text=filename if filename else "No file uploaded")
+
+          previewBox.delete("0.0", "end")
+          previewBox.insert("0.0", data.head().to_string()) #Inserts the df.head into the textbox
+
      else:
           filenameText.configure(text="Please upload a CSV file.")
+
+
+def selectGraphCombo(choice):
+    global selected_graph_type
+    selected_graph_type = choice
+    print("Selected graph type:", selected_graph_type)
+
+def previewData():
+     tb.Table()
+
 
 uploadFilebtn = ct.CTkButton(app, text="Upload CSV", command=load_csv)
 uploadFilebtn.pack()
 
 filenameText = ct.CTkLabel(app, text_color="black", fg_color="transparent", text=filename)
 filenameText.pack()
+
+selectGraphComboBox = ct.CTkComboBox(app, values=["Line", "Bar", "Scatter", "Pie"], command=selectGraphCombo)
+selectGraphComboBox.pack()
+
+previewBox = ct.CTkTextbox(app, width=800, height=200)
+previewBox.pack()
 
 
 
